9424fe65844d494ba5468298e14ba66694c762a1
Henrik Juhl <henrik.juhl_dk@hotmail.com>
2023-11-10 20:51:56 +0100

    Email TagHelper etc

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -14,10 +14,21 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string category)
         {
-            var vm = new PieListViewModel(_pieRepository.AllPies, "All pies");
-            return View(vm);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+            if (string.IsNullOrEmpty(category) )
+            {
+                pies = _pieRepository.AllPies.OrderBy(c => c.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName.Equals(category)).OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+             }
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id) { 
--- Program.cs
+++ Program.cs
@@ -4,15 +4,24 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<IShoppingCart,  ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor(); 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection")));
+
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 DbInitializer.Seed(app);
 app.Run();
